82034165056ccf5e43d90bd2f2c2747966a20145
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-08-30 13:40:31 -0500

    header fixing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ import Navigation from './Navigation';
 export default function Header({currentPage, handlePageChange}){
  
     return (
-        <header>
+        <header className=''>
+            
             <h1> Elena F</h1>
             <Navigation currentPage={currentPage}  handlePageChange={handlePageChange} />
         </header>
