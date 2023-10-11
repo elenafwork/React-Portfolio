040d8c06f71e390337c49dc156096c3bc79beca3
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-10-11 18:05:55 -0500

    linkdin link

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -17,7 +17,7 @@ export default function Contact(){
                     <ul>
                     <li><a href="mailto:elenaf.work@gmail.com"><img src="https://i.imgur.com/bAYdVtT.png" alt='email icon' style={styles} /></a></li>
                     <li><a href="https://github.com/elenafwork"><img src="https://i.imgur.com/tryNfW0.png" alt='gitHub icon' style={styles} /></a></li>
-                    <li> <a href="https://www.instagram.com/elenaf_art/" ><img src="https://i.imgur.com/XsH0KVA.png" alt='linkdin icon' style={styles} /></a></li>
+                    <li> <a href="https://www.linkedin.com/in/elena-fadeeva-ef/" ><img src="https://i.imgur.com/XsH0KVA.png" alt='linkdin icon' style={styles} /></a></li>
                     <li><a href="https://www.instagram.com/elenaf_art/"><img src="https://i.imgur.com/3GSfTF5.png" alt='instagram icon' style={styles} /></a></li>
                     </ul>
                 
